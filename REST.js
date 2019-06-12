53720fcee0f7b2c81e9d138f3924c1082340dea1
Athreya Sridharan <athreyas@andrew.cmu.edu>
2019-06-11 22:37:01 -0400

    Changes to API to work with new DB schema

--- REST.js
+++ REST.js
@@ -25,54 +25,54 @@
 *
 ******************************************************************************************************************/
 
-var mysql   = require("mysql");     //Database
+var mysql = require("mysql");     //Database
 var utils = require("./utils.js");
 
-function REST_ROUTER(router,connection) {
+function REST_ROUTER(router, connection) {
     var self = this;
-    self.handleRoutes(router,connection);
+    self.handleRoutes(router, connection);
 }
 
 // Here is where we define the routes. Essentially a route is a path taken through the code dependent upon the 
 // contents of the URL
 
-REST_ROUTER.prototype.handleRoutes= function(router,connection) {
+REST_ROUTER.prototype.handleRoutes = function (router, connection) {
 
     // GET with no specifier - returns system version information
     // req paramdter is the request object
     // res parameter is the response object
 
-    router.get("/",function(req,res){
-        res.json({"Message":"Orders Webservices Server Version 1.0"});
+    router.get("/", function (req, res) {
+        res.json({ "Message": "Orders Webservices Server Version 1.0" });
     });
 
     // Modified by CJ - 04/26/2019
     // GET for /now - returns the current simulation time of the server.
 
-    router.get("/now", function(req, res) {
-        res.json({"Now": utils.now().toLocaleString()});
+    router.get("/now", function (req, res) {
+        res.json({ "Now": utils.now().toLocaleString() });
     });
 
     // GET for /timeconfig - returns the simulation configuration.
 
-    router.get("/timeconfig", function(req, res) {
+    router.get("/timeconfig", function (req, res) {
         res.json(utils.getConfig());
     });
-    
+
     // GET for /pending specifier - returns all pending orders currently stored in the database
     // req paramdter is the request object
     // res parameter is the response object
-  
-    router.get("/pending",function(req,res){
-        console.log("Getting all database entries..." );
+
+    router.get("/pending", function (req, res) {
+        console.log("Getting all database entries...");
         var query = "SELECT * FROM ?? WHERE ??=?";
-        var table = ["orders", "pending", true];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "status", "pending"];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Success", "Orders" : rows});
+                res.json({ "Error": false, "Message": "Success", "Orders": rows });
             }
         });
     });
@@ -80,17 +80,17 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // GET for /pending/id specifier - returns the pending order for the provided order ID
     // req paramdter is the request object
     // res parameter is the response object
-     
-    router.get("/pending/:id",function(req,res){
-        console.log("Getting order ID: ", req.params.id );
+
+    router.get("/pending/:id", function (req, res) {
+        console.log("Getting order ID: ", req.params.id);
         var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
-        var table = ["orders","id", req.params.id,"pending",true];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "orderID", req.params.id, "status", "pending"];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+                res.json({ "Error": false, "Message": "Success", "Users": rows });
             }
         });
     });
@@ -98,17 +98,17 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // GET for /pendingcustorders specifier - returns the order for the provided order ID
     // req paramdter is the request object
     // res parameter is the response object
-     
-    router.get("/pendingcustorders/:customer",function(req,res){
-        console.log("Getting orders for: ", req.params.customer );
+
+    router.get("/pendingcustorders/:customer", function (req, res) {
+        console.log("Getting orders for: ", req.params.customer);
         var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
-        var table = ["orders","customer",req.params.customer,"pending",true];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "customerID", req.params.customer, "status", "pending"];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+                res.json({ "Error": false, "Message": "Success", "Users": rows });
             }
         });
     });
@@ -116,17 +116,17 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // GET for /filled specifier - returns all filled orders currently stored in the database
     // req paramdter is the request object
     // res parameter is the response object
-  
-    router.get("/filled",function(req,res){
-        console.log("Getting all database entries..." );
+
+    router.get("/filled", function (req, res) {
+        console.log("Getting all database entries...");
         var query = "SELECT * FROM ?? WHERE ??=?";
-        var table = ["orders", "pending", false];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "status", "filled"];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Success", "Orders" : rows});
+                res.json({ "Error": false, "Message": "Success", "Orders": rows });
             }
         });
     });
@@ -134,17 +134,17 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // GET for /filledcustorders specifier - returns the filled orders for the specified customer
     // req paramdter is the request object
     // res parameter is the response object
-     
-    router.get("/filledcustorders/:customer",function(req,res){
-        console.log("Getting orders for: ", req.params.customer );
+
+    router.get("/filledcustorders/:customer", function (req, res) {
+        console.log("Getting orders for: ", req.params.customer);
         var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
-        var table = ["orders","customer",req.params.customer,"pending",false];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "customerID", req.params.customer, "status", "filled"];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+                res.json({ "Error": false, "Message": "Success", "Users": rows });
             }
         });
     });
@@ -153,21 +153,21 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // This moves the order specified by ID to the fulfilled table.
     // req paramdter is the request object
     // res parameter is the response object
-     
-    router.post("/markOrderFilled/:id",function(req,res){
+
+    router.post("/markOrderFilled/:id", function (req, res) {
         console.log("Marking fulfilled order ID: ", req.params.id);
         var query = "UPDATE ?? SET ??=?, ??=? WHERE ??=?"
         // var table = ["orders", "pending", false, "filldate", new (Date), "id", req.params.id];
         // Modified by CJ - 04/25/2019
         // Use the simulation time to write database.
-        var table = ["orders", "pending", false, "filldate", utils.now(), "id", req.params.id];
+        var table = ["orders", "status", "filled", "fulfillTime", utils.now(), "orderID", req.params.id];
 
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Order marked filled", "Users" : rows});
+                res.json({ "Error": false, "Message": "Order marked filled", "Users": rows });
             }
         });
     });
@@ -175,20 +175,20 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // POST for adding orders
     // req paramdter is the request object - note to get parameters (eg. stuff afer the '?') you must use req.body.param
     // res parameter is the response object 
-  
-    router.post("/neworder",function(req,res){
-        console.log("Adding order::", req.body.customer,",",req.body.red,",",req.body.blue,",",req.body.green,",",req.body.yellow,",",req.body.black,",",req.body.white);
+
+    router.post("/neworder", function (req, res) {
+        console.log("Adding order::", req.body.customer, ",", req.body.red, ",", req.body.blue, ",", req.body.green, ",", req.body.yellow, ",", req.body.black, ",", req.body.white);
         // Modified by CJ - 04/25/2019
         // Use the simulation time to write database.
         var query = "INSERT INTO ??(??,??,??,??,??,??,??,??,??) VALUES (?,?,?,?,?,?,?,?,?)";
-        var table = ["orders","customer","red","blue","green","yellow","black","white","pending", "orderdate",
-            req.body.customer,req.body.red,req.body.blue,req.body.green,req.body.yellow,req.body.black,req.body.white,true, utils.now()];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "customer", "red", "blue", "green", "yellow", "black", "white", "pending", "orderdate",
+            req.body.customer, req.body.red, req.body.blue, req.body.green, req.body.yellow, req.body.black, req.body.white, true, utils.now()];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "User Added !"});
+                res.json({ "Error": false, "Message": "User Added !" });
             }
         });
     });
@@ -196,21 +196,20 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // DELETE for deleting a pending order as satisfied for the provided order ID
     // req paramdter is the request object
     // res parameter is the response object
-     
-    router.delete("/deleteOrder/:id",function(req,res){
+
+    router.delete("/deleteOrder/:id", function (req, res) {
         console.log("Deleting order ID: ", req.params.id);
         var query = "DELETE FROM ?? WHERE ??=?";
-        var table = ["orders", "id", req.params.id];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        var table = ["orders", "ordersID", req.params.id];
+        query = mysql.format(query, table);
+        connection.query(query, function (err, rows) {
+            if (err) {
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
-                res.json({"Error" : false, "Message" : "Delete order OK", "Users" : rows});
+                res.json({ "Error": false, "Message": "Delete order OK", "Users": rows });
             }
         });
     });
-
 
 }
 
